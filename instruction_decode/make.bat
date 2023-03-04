174f51d6b3db8a0f3992cfd736defa8d4c2d978d
Said Al Attrach <alattrach.said@yahoo.com>
2023-03-04 05:56:57 +0100

    Dynamically link against crt

--- instruction_decode/make.bat
+++ instruction_decode/make.bat
@@ -1,4 +1,4 @@
 @echo off
-nasm -o main.obj -f win64 -g main.asm
+nasm -g -o main.obj -f win64 main.asm
 "\Program Files\Microsoft Visual Studio\2022\Community\VC\Tools\MSVC\14.34.31933\bin\HostX64\x64\link.exe" ^
-	main.obj /debug /nologo /out:main.exe /subsystem:console libcmt.lib
+	main.obj /nologo /debug /out:main.exe /subsystem:console msvcrt.lib ucrt.lib legacy_stdio_definitions.lib
