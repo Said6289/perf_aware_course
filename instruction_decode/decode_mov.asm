30cf60103c4ea52da3e82c067722df058714db4b
Said Al Attrach <alattrach.said@yahoo.com>
2023-03-05 21:58:41 +0100

    Getting there

--- instruction_decode/decode_mov.asm
+++ instruction_decode/decode_mov.asm
@@ -31,9 +31,35 @@ decode_register_to_register:
 	; save second byte of instruction, now whole instruction is in bx
 	mov bh, al
 
-	and al, 0b11000000 ; mask out MOD field
-	cmp al, 0b11000000 ; check if is a register to register mov
-	jne print_unknown_instruction
+	; check MOD field for how many more bytes to fetch
+	mov r12, rbx
+	and r12, 0b1100000000000000 ; mask out MOD field
+	shr r12, 14
+
+	; counter for number of bytes to fetch
+	mov r13, r12
+	cmp r13, 2
+	mov rax, 0
+	cmovg r13, rax ; fetch maximum of 2 bytes
+
+	; fetch al bytes
+	.fetch:
+	; loop condition
+	cmp r13, 0
+	jle .fetch_exit
+	; read next byte
+	mov rcx, rsi
+	call fgetc
+	; or byte into ebx
+	and eax, 0xFF
+	mov ecx, 2
+	sub ecx, r12d
+	shl eax, cl
+	or  ebx, eax
+	; decrement number of bytes
+	sub r13, 1
+	jmp .fetch
+	.fetch_exit:
 
 	mov si, bx
 	and rsi, 0b0011100000000000 ; mask out REG field
@@ -42,6 +68,10 @@ decode_register_to_register:
 	mov di, bx
 	and rdi, 0b0000011100000000 ; mask out R/M field
 	shr di, 8
+
+	; check if it is the memory to register case
+	cmp r12, 0b11
+	jne decode_memory_to_register
 
 	mov al, bl
 	mov ah, bl
@@ -61,16 +91,36 @@ decode_register_to_register:
 
 	lea rax, [reg_table_w0]
 
-	lea rcx, [mov_instruction]
+	lea rcx, [mov_reg_to_reg]
 	lea rdx, [rax + 4 * rsi]
 	lea r8,  [rax + 4 * rdi]
 	call printf
 
-	; bh contains the last byte we read from the file
-	xor rax, rax
-	mov al, bh
+	jmp decode_mov_return_value
 
-	jmp decode_mov_return
+decode_memory_to_register:
+	mov al, bl
+	mov ah, bl
+	and al, 0b01 ; mask out W bit
+	and ah, 0b10 ; mask out D bit
+
+	shl al, 3 ; offset in terms of number of elements
+
+	; set bit 3 which effectively determines the table
+	or sil, al
+
+	lea rcx, [mov_mem_to_reg]
+
+	lea rax, [reg_table_w0]
+	lea rdx, [rax + 4 * rsi]
+
+	lea rax, [disp_table]
+	imul rdi, 8 ; stride by 8
+	lea r8,  [rax + rdi]
+
+	call printf
+
+	jmp decode_mov_return_value
 
 decode_immediate_to_reg:
 	and al, 0b11110000
@@ -104,30 +154,33 @@ decode_immediate_to_reg:
 	mov di, bx
 	and rdi, 0b111
 
-	shl al, 3 ; offset in terms of number of elements
+	mov al, bl
+	and al, 0b00001000 ; mask out W bit
 
 	; set bit 3 which effectively determines the table
 	or dil, al
 
 	lea rax, [reg_table_w0]
 
-	lea rcx, [mov_immediate_to_reg]
+	lea rcx, [mov_imm_to_reg]
 	lea rdx, [rax + 4 * rdi]
 	mov r8d, ebx
 	shr r8d, 8
 	call printf
 
-	; bh contains the last byte we read from the file
-	xor rax, rax
-	mov al, bh
-
-	jmp decode_mov_return
+	jmp decode_mov_return_value
 
 print_unknown_instruction:
 	lea rcx, [unknown_instruction]
 	call printf
 
 	mov rax, -1
+	jmp decode_mov_return
+
+decode_mov_return_value:
+	; bh contains the last byte we read from the file
+	xor rax, rax
+	mov al, bh
 
 decode_mov_return:
 	add rsp, 32
--- instruction_decode/strings.asm
+++ instruction_decode/strings.asm
@@ -1,10 +1,26 @@
 usage db "USAGE: %s <file>", 0xd, 0xa, 0
-file_not_found db "File not found.", 0xd, 0xa, 0
-unknown_instruction db 0x25, 0x25, `error "encountered unknown instruction"`, 0xd, 0xa, 0
 mode db "rb", 0
+file_not_found db "File not found.", 0xd, 0xa, 0
+
 first_line db "; %s disassembly:", 0xd, 0xa, 0
 second_line db "bits 16", 0xd, 0xa, 0
+
+unknown_instruction db 0x25, 0x25, `error "encountered unknown instruction"`, 0xd, 0xa, 0
+
 reg_table_w0 db "al", 0, 0, "cl", 0, 0, "dl", 0, 0, "bl", 0, 0, "ah", 0, 0, "ch", 0, 0, "dh", 0, 0, "bh", 0, 0
 reg_table_w1 db "ax", 0, 0, "cx", 0, 0, "dx", 0, 0, "bx", 0, 0, "sp", 0, 0, "bp", 0, 0, "si", 0, 0, "di", 0, 0
-mov_instruction db "mov %s, %s", 0xd, 0xa, 0
-mov_immediate_to_reg db "mov %s, %u", 0xd, 0xa, 0
+
+disp_table:
+	db "bx + si", 0
+	db "bx + di", 0
+	db "bp + si", 0
+	db "si", 6 dup byte (0)
+	db "di", 6 dup byte (0)
+	db "bp", 6 dup byte (0)
+	db "bx", 6 dup byte (0)
+
+mov_reg_to_reg      db "mov %s, %s",         0xd, 0xa, 0
+mov_mem_to_reg      db "mov %s, [%s]",      0xd, 0xa, 0
+mov_mem_to_reg_disp db "mov %s, [%s + %d]", 0xd, 0xa, 0
+
+mov_imm_to_reg db "mov %s, %u", 0xd, 0xa, 0
